51a4649668ecff1bc0430e837ef756e65737e62e
Changhoon Ji <cji2@gmu.edu>
2019-10-13 22:46:38 -0400

    Added Entity Framework

---
+++ MyShop/MyShop.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyShop.Core.Models;
+
+namespace MyShop.DataAccess.SQL
+{
+    public class DataContext : DbContext
+    {
+        /* DefaultConnection is the name of connectionStrings element of Web.config file on MyShop.WebUI project. */
+        public DataContext()
+            : base("DefaultConnection") {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+    }
+}
