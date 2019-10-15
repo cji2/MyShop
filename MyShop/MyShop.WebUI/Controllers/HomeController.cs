abdefbde075c93190addf107f971275dc2318214
Changhoon Ji <cji2@gmu.edu>
2019-10-15 14:56:56 -0400

    Product List Added.

--- MyShop/MyShop.WebUI/Controllers/HomeController.cs
+++ MyShop/MyShop.WebUI/Controllers/HomeController.cs
@@ -3,14 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MyShop.Core.Contracts;
+using MyShop.Core.Models;
 
 namespace MyShop.WebUI.Controllers
 {
     public class HomeController : Controller
     {
+
+        // the following has the property, List<Product> products.
+        IRepository<Product> context;
+        //InMemoryRepository<Product> context;
+        //ProductRepository context;
+
+        // newly added following has the property, List<ProductCategory> productCategories
+        IRepository<ProductCategory> productCategories;
+        //ProductCategoryRepository productCategories;
+
+        // initialize the repository, which will call the constructor, ProductRepository()
+        public HomeController(IRepository<Product> productContext, IRepository<ProductCategory> productCategoryContext)
+        {
+            /* the class, ProductRepository has the property, List<Product> products. */
+            context = productContext;
+            //context = new InMemoryRepository<Product>();
+
+            // newly added following has the property, List<ProductCategory> productCategories
+            productCategories = productCategoryContext;
+            //productCategories = new InMemoryRepository<ProductCategory>();
+        }
+
         public ActionResult Index()
         {
-            return View();
+            /* method Collection() returns IQueryable<Product>. */
+            List<Product> products = context.Collection().ToList();
+            return View(products);
+        }
+
+        public ActionResult Details(string Id)
+        {
+            Product product = context.Find(Id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(product);
+            }
         }
 
         public ActionResult About()
