eeea7d8741e9fe708e916fdf67a147fe77ed61e4
Changhoon Ji <cji2@gmu.edu>
2019-10-22 16:57:35 -0400

    Revised Customer Model with updating database (add-migration)

--- MyShop/MyShop.Core/Models/Customer.cs
+++ MyShop/MyShop.Core/Models/Customer.cs
@@ -13,6 +13,7 @@ namespace MyShop.Core.Models
         public string LastName { get; set; }
         public string Email { get; set; }
         public string Street { get; set; }
+        public string City { get; set; }
         public string State { get; set; }
         public string ZipCode { get; set; }
     }
